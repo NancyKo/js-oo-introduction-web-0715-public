c099a79a6052b222669652081cc3e9ace6525765
NancyKo <konancys@gmail.com>
2015-09-17 19:36:35 -0400

    Done

--- js/bike.js
+++ js/bike.js
@@ -1,7 +1,25 @@
-function Bike(){
+function Bike(name, bikeType, frameColor){
+  this.name = name;
+  this.bikeType = bikeType;
+  this.frameColor = frameColor;
+  this.condition = 'ready to go!';
+  this.WHEELS = 2;
   // attributes go here...
+
 }
 
-Bike.prototype.someInstanceMethod = function(){
+Bike.prototype.takeForARide = function(miles){
   // code goes here...
+    if (miles > 50) {
+      this.condition = 'needs a tune up';
+    } 
 };
+
+Bike.prototype.tuneUp = function(){
+  this.condition = 'ready to go!';
+};
+
+
+
+
+
--- js/owner.js
+++ js/owner.js
@@ -1,7 +1,19 @@
-function Owner(){
-  // attributes go here...
+function Owner(name){
+    this.name = name;
+    this.bikes = [];
 }
 
-Owner.prototype.someInstanceMethod = function(){
+Owner.prototype.buildBike = function(name, type, color){
   // code goes here...
+  var b = new Bike(name, type, color);
+  this.bikes.push(b);
 };
+
+Owner.prototype.rideBike = function(name, miles) {
+  var ride = new Bike(name, miles);
+  this.bikes.forEach(function(bike){
+    if (bike.name === name){
+      bike.condition = "needs a tune up";
+    }
+  });
+}
